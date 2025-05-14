b389c319050637fa3f8426f54c8edf6e03dbf24f
Craig Barcinas <cbarcinas@live.com>
2025-05-14 15:52:57 -0700

    added markup and styles

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,5 +1,12 @@
+import Header from '../components/Header';
+
 const Layout = () => {
-  return <div className="flex flex-col min-h-screen"></div>;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div></div>
+    </div>
+  );
 };
 
 export default Layout;
