77af67bb5842fc5bb4128715cb5efc56f0b61a72
Craig Barcinas <cbarcinas@live.com>
2025-05-17 21:27:46 -0700

    added styles

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,12 +5,12 @@ const Header = () => {
     <div className="bg-blue-800 py-6">
       <div className="container mx-auto flex justify-between">
         <span className="text-3xl text-white font-bold tracking-tight">
-          <Link to="/">BookMeRN.com</Link>
+          <Link to="/">BookMern.com</Link>
         </span>
         <span className="flex space-x-2">
           <Link
             to="/sign-in"
-            className="flex items-center text-blue-600 px-3 font-bold hover:bg-gray-300"
+            className="flex items-center text-blue-600 px-3 font-bold hover:bg-gray-300 bg-white"
           >
             Login
           </Link>
