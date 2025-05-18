3ec348ebd21902bf23ca96a4c418650ba41b3a59
Craig Barcinas <cbarcinas@live.com>
2025-05-17 21:27:54 -0700

    wip

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <div className="bg-blue-800 pb-16">
+      <div className="container mx-auto flex flex-col gap-2">
+        <h1 className="text-5xl text-white font-bold">
+          Find your next destination hideaway !
+        </h1>
+        <p className="text-2xl text-white">
+          Find low prices on hotels to make your trip more enjoyable . . .
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
