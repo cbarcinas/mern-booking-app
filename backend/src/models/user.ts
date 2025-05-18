f10dbc2e60b70f6c2b9a00e496e5d40b739d1fe7
Craig Barcinas <cbarcinas@live.com>
2025-05-18 16:03:59 -0700

    created ts user type

---
+++ backend/src/models/user.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+// Typescript type
+export type UserType = {
+  _id: string;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+};
+
+
