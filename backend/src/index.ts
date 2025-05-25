f20b4f248f15f5a265309bd4d7287b6cd446fc60
Craig Barcinas <cbarcinas@live.com>
2025-05-24 18:49:40 -0700

    add register route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
+import registerRouter from './routes/users';
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -9,6 +10,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use('/api/register', registerRouter);
 
 app.get('/api/test', async (req: Request, res: Response) => {
   res.json({ message: 'hello from endpoint' });
